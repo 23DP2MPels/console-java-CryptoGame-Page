abf48160a16aa07da6734cf2a971920d504e04f7
Mark Tatu <a231453mt@rvt.lv>
2025-04-08 19:33:21 +0300

    add 3d model (no respons)

---
+++ src/js/main.js
@@ -0,0 +1,69 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75,
+  window.innerWidth / window.innerHeight, 
+  0.1,
+  100
+)
+
+const loader = new GLTFLoader()
+loader.load(
+  'src/models/bitcoin/scene.gltf',
+  (gltf) => {
+    const model = gltf.scene
+    model.scale.set( window.innerWidth*0.003, window.innerWidth*0.003, window.innerWidth*0.0015)
+    
+    object = gltf.scene;
+    scene.add(model)
+  }
+)
+
+let object;
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+// const dirLight = new THREE.DirectionalLight('white', 1)
+// dirLight.position.set(5,5,5)
+// scene.add(dirLight)
+
+const light = new THREE.PointLight('white',100,100)
+light.position.set(0,5,10)
+scene.add(light)
+
+// const lightHelp = new THREE.lightHelp(light, 0.5)
+// scene.add(lightHelp)
+
+camera.position.z = 5
+
+const renderer = new THREE.WebGLRenderer({ alpha: true })
+renderer.setSize(600, 600)
+
+// document.body.appendChild(renderer.domElement)
+
+document.getElementById("container3D").appendChild(renderer.domElement)
+
+renderer.setClearColor( 0x000000, 0 )
+
+
+function animate() {
+  requestAnimationFrame(animate)
+  // object.position.y = -mouseY /  window.innerHeight * 0.1 ;
+  // object.position.x = mouseX / window.innerWidth * 0.1;
+  object.rotation.z = -0.2 + (mouseX / window.innerWidth * 0.2);
+  object.position.y =-(mouseY / window.innerHeight * 0.4);
+  object.rotation.x = -1.8 // + (mouseY / window.innerHeight * 0.4);
+  renderer.render(scene, camera)
+}
+
+document.onmousemove = (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+}
+
+
+
+animate()
